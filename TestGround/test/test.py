1fe25b8128f00f4dc6a9a621166cf4a5426f4c49
Anna Nelles <anna.nelles@gmail.com>
2019-05-21 11:07:35 +0200

    More imports

--- TestGround/test/test.py
+++ TestGround/test/test.py
@@ -3,5 +3,7 @@
 import numpy as np
 import astropy
 from radiotools import coordinatesystems
+from __future__ import absolute_import, division, print_function
+from NuRadioReco.utilities import units
 
 print("Nothing to do")
