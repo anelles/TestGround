9ceda8f84b12c781dc8fb805b95e17bfa226fd7c
Anna Nelles <anna.nelles@gmail.com>
2019-05-21 11:10:47 +0200

    So many things to think of.

--- TestGround/test/test.py
+++ TestGround/test/test.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import, division, print_function
 import numpy as np
 import astropy
 from radiotools import coordinatesystems
-from __future__ import absolute_import, division, print_function
 from NuRadioReco.utilities import units
+import argparse
+import NuRadioReco.modules.channelGenericNoiseAdder
+from NuRadioMC.simulation import simulation2 as simulation
+import logging
 
 print("Nothing to do")
